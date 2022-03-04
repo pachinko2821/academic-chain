119091a74dd7e270d773c7287bf40f5a6624a928
pachinko2821 <pachinko28@protonmail.com>
2022-03-04 13:19:53 +0530

    Added PGP signature and PDF generation

--- src/app.js
+++ src/app.js
@@ -166,10 +166,80 @@ App = {
                 title: 'Student Found',
                 text: ''.concat('Name: ',data['name'], '\nDate of Birth: ', date, '\nResult: ', data['result']),
                 icon: 'success',
-                confirmButtonText: 'Close'
+                confirmButtonText: 'Download PDF'
+            }).then(() => {
+                App.generate_pdf(data)   
             })
         }
     },
+    generate_pdf: async (data) => {
+        // https://github.com/openpgpjs/openpgpjs#encrypt-and-decrypt-string-data-with-pgp-keys                                                  
+        (async function() {                                                                                                                      
+        try{                                                                                                                                 
+        // put keys in backtick (``) to avoid errors caused by spaces or tabs                                                            
+        const publicKeyArmored = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: Keybase OpenPGP v2.1.15
+Comment: https://keybase.io/crypto
+
+xsBNBGIghaoBCACpfvzmZ5Vt/u0Q27ecEmcYDALhK/Nv12zgovm7VkmnNyeqcbYE
+ou3K7+nDLiu/1oXkX3Qh921swuAtimv58AWoT6Gj235rRNEvw327h7hG1oJukPds
+nt2Yat0WTmP+N5N/+FiJMs4i53B2QJAbh4xhyi8+s1O4PJ4fu9bGSp+8+R3WOGTW
+OwJg+M6zdmvOY00It1C6PwvMQozY9RFh4sTwSyRrzaZNpZfU0i+gPIFI6ILhlRLT
+k35GprbuD5hAd2G3KvfOhWJN5yCadLh7H/c735jczDYS/0mrQPwKWmq73DD0tHkt
+qF7JCdga2AxbKvMmgruplJLGn2tTKuzwCl/BABEBAAHNAMLAdAQTAQoAHgUCYiCF
+qgIbLwMLCQcDFQoIAh4BAheAAxYCAQIZAQAKCRBmGaZc5WZC34IJB/4z2ms4s8WL
+8d4H2PlOkmKepyti/4Z76RkVk46B6GY/l68LYSvqgrkAtqmpx46pVBe9jrXgcKD/
+iMhkb2M7F1dHdeD/zfznd3TGdQcqeASOpdqZ2UFjqGxFVfLxMzAfGSxfwow5qYzv
+QMBp8NQJ3FQCr0nA62LcVAwPZrfM4F3OUNNft0wpDlbMdy9M9uWE2ITrZiPSoiE/
+kE37zC/SEW8BIQ/l0OUqht8X0NM/2jJJElZsJViof8LP9UkZB0d7Zy2li3McXFZi
+3FEIFThDoZJ20QyfQI9SFxuiJW2KkglYWNC7ymwPTSl/K+R9ec0i3fjULYeF/6ux
+Y0cFkVW9eUWJzo0EYiCFqgEEANMs7jO6n2RY8FzfwKR2KJPz5/1DYrjzWhstMTCj
+HESCFGEA4g/5W9zK3jh1VoOTF2nMbHzn856/vF1Wd5gyhhA48NEYP7FahFsVhlyP
+Ei3IWL0BfSfhV3ejRT/VNZfW4dg26F6XvwxxkuB955WU1ByE3chqhyWjVAs0lWri
+HbhxABEBAAHCwQMEGAEKAA8FAmIghaoFCQ8JnAACGy4AqAkQZhmmXOVmQt+dIAQZ
+AQoABgUCYiCFqgAKCRAxkpxwatkLqyaCBACAY5d/rTh/XxP+tKzA4E/o/7nz0DyW
++mffZAj9a/CPFH9tfo+9VVk+8PsC1YJtlvetFcOrVjVfxJPFBqZRO8DI0/KyFrTp
+Qdx1K7+PFYB/txzBmErCU26wWdgW1Csbmp7+FeEGyOLMS/avvBfDcqNpm+8Fgs/+
+mLz3suoWeGu35dTQB/9tmCsbUAdGrpJ82ETM6leITl3wETbPAmv4LHgRZzkTZIxd
+J0NkSguNmdieQ6yGtnz5+mrVK8DaZWee0I7ZbVVKjOA9vnpCV1uA6EbxZWPL7Aaa
+Ntc1XtEJQBhZPFvMGdqjQjTLUr5+IJ34ueq4UA5Sn5d0EAClzx63jSYwmUPtcX1z
+SEYZKAjgKR7t+N6bKcGRs6JkugGXM3jjaXbEtnyJgcY/61qYka1ElDtKWrh6G26B
+XG8jv2TWtf9L2ykdiaFT6C9JBt5RBelnTrPPn3BKKpXV85ANv5WlVOSbIYssDU2i
+0V2t8+nmKPVM4OGHGrbmU1rIvcRtenKF7evali02zo0EYiCFqgEEALqSdY4wj9rI
+m5tZ3DtqmSJspI/0magJUafzTHz8BpCIBJ3rMDN1xvvsLG1aWaeGhUqeSjBj7Y/D
+qjTDhZvSWz6JfoV8Y3UjnpoSp/djhOPMHhNIoXvKkwzRmQX0KaRDWsekAZsb9thj
+jqitSyJouqlL4MS+WmT+jOFC0Cf6BSR5ABEBAAHCwQMEGAEKAA8FAmIghaoFCQPC
+ZwACGy4AqAkQZhmmXOVmQt+dIAQZAQoABgUCYiCFqgAKCRCLDQe0Cv7wj3qOA/9I
+1uq1vi8+sEzJ1ilOwNIK1vdw13u8FdkYZIjbvmiR/kHK+4zFRweA8jh1j1EOwW1/
+gvRCHXS2XmOff4MCcGJzaYiRFqcSk478Ioiy0U05Lp9T1GUPBHk3aUO16goDv9Sn
+HKS9Jvtr73BQSba3Pb/kOGh3VpnBA3kbIiU8BUCT7OS2B/9ATd7kx2MttHr0pCGQ
+fFbb09l6+nhrRFWlnJKQqMXh83w78L9Yz76s/ySN2KGgNrzpqRqwamlyumB7o+G5
+wniYw7Qm05TdAv2MnEKPKQMJdKgMKCgQDB7t9MLMkvKf3dKTeG6GJMYG7yxFoDo5
+by9ERsGlfJYcZBpwYdsRZYG/EB/zF52xNDu6gUa+gk4GiFdhN3SpmeRVfB/cX2Bx
+fXg3b820vwGYV8bld6J+ON7+puqjp0ygtaaIZqsnWysFrpHAD2YdYrJP5hn0MfKT
+X5F7XudPlIch1mcfF7xL+zcDZv33Fftu6X//Oxl3C9Lwbf3hqLoNkNCOPrW4Bmgz
+FZOa
+=R2k5
+-----END PGP PUBLIC KEY BLOCK-----`;
+        //const privateKeyArmored = ``; // encrypted private key                                                                         
+                                                                                                                                
+        const message = JSON.stringify(data);                                                                                            
+        const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });                                                       
+        const encrypted = await openpgp.encrypt({                                                                                        
+        message: await openpgp.createMessage({ text: message }), // input as Message object                                          
+        encryptionKeys: publicKey,                                                                                                   
+        });                                                                                                                              
+        
+        console.log(encrypted);                                                                                                          
+        data['pgp'] = btoa(encrypted);                                                                                                         
+        window.open("templates/template.html?data="+btoa(JSON.stringify(data)));
+
+        }                                                                                                                                    
+        catch(e){                                                                                                                            
+        console.log("error: ", e); 
+        }
+        })();
+    }
 }
 
 $(() => {
